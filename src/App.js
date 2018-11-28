439675308fd51b5695f14eea6dea4067e7702d94
Patric Khiev <webdev.patric@gmail.com>
2018-11-27 23:41:17 -0600

    project completed

--- src/App.js
+++ src/App.js
@@ -2,33 +2,45 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    sideNav: false,
+    links: ["SERVICES", "PORTFOLIO", "ABOUT", " TEAM", "CONTACT"]
+  };
+
   render() {
+    const { sideNav, links } = this.state;
+    const linkMap = links.map((e, i) => {
+      return (
+        <div key={i}>
+          <p>{e}</p>
+        </div>
+      );
+    });
     return (
       <div className="App">
-        <div className="landing_page">
-          <div className="App_header">
-            <div className="header_container_left">
-              <p>Start Bootstrap</p>
-            </div>
-            <div className="header_container_right">
-              <p>SERVICES</p>
-              <p>PORTFOLIO</p>
-              <p>ABOUT</p>
-              <p>TEAM</p>
-              <p>CONTACT</p>
-            </div>
-          </div>
-          <div className="body">
-            <div className="inner_body">
-              <h2>Welcome To Our Studio!</h2>
-              <h1>IT'S NICE TO MEET YOU</h1>
-              <button className="tell_button">TELL ME MORE</button>
+        <nav className="nav">
+          <h2 className="title">Start Bootstrap</h2>
+          <div className="menu">
+            <div className="main_nav">{linkMap}</div>
+            <div className="hamburger">
+              <div
+                className="hamburger-icon"
+                onClick={() => this.setState({ sideNav: !sideNav })}
+              >
+                <div className="burger burger1" />
+                <div className="burger burger1" />
+                <div className="burger burger1" />
+              </div>
+              {sideNav && <div className="sidenav">{linkMap}</div>}
             </div>
           </div>
+        </nav>
+        <div className="main">
+          <h2>Welcome to our studio!</h2>
+          <h1>It's Nice To Meet You!</h1>
         </div>
       </div>
     );
   }
 }
-
 export default App;
